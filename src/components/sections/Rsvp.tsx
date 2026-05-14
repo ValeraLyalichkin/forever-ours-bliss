6fd46acfde7f848f9b290d57a90b44a87d61cb22
Валерий Ляличкин <95354732+ValeraLyalichkin@users.noreply.github.com>
2026-05-14 09:26:15 +0300

    Update Rsvp.tsx

--- src/components/sections/Rsvp.tsx
+++ src/components/sections/Rsvp.tsx
@@ -5,7 +5,7 @@ import { launchHearts } from "@/lib/hearts";
 import { SketchCorners } from "../SketchCorners";
 
 // Вставьте сюда URL вашего Google Apps Script Web App (см. инструкцию ниже)
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyKmlqBjlOoGXHLw4dHyQ9STrXVxItVw-Qrouzz4zNVDmnbrT1QTGxNpb63L_2t-KFpXg/exec";
 
 const ALCOHOL = ["Белое вино", "Красное вино", "Виски", "Водка", "Самогон"];
 
